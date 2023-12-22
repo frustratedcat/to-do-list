221d2e24d95a867bb6aa175b4a8d8d8f992dd81e
frustratedcat <jaredbrown@skiff.com>
2023-12-22 13:17:45 -0500

    Add inital js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,48 @@
 'use strict';
 
 import './style.css';
+
+
+// Create toDo factory function, which will be called within project arrays
+function createToDo(title, description, dueDate, priority, notes) {
+  const toDo = {
+    title: title,
+    description: description,
+    dueDate: dueDate,
+    priority: priority,
+    notes: notes,
+  }
+
+  return toDo
+}
+
+// Create project factory function
+function createNewProject(name) {
+  const projectName = [];
+  return projectName;
+}
+
+// Add toDo to project
+function addToDo(name, title, description, dueDate, priority, notes) {
+  const newProject = createNewProject(name);
+  const newToDo = createToDo(title, description, dueDate, priority, notes);
+
+  newProject.push(newToDo);
+  return newProject;
+}
+
+// Create projects container array
+function projectsContainer() {
+  const projects = [];
+  return {projects};
+}
+
+// Add new projects and toDos to container
+function addProjectsAndToDos() {  
+  const allProjects = projectsContainer();
+  allProjects.projects.push(addToDo('default', 'first to do', 'a bunch of words', 'sometime' ,'high', 'none'));
+
+  return allProjects;
+}
+
+console.log(addProjectsAndToDos());
