b2d1d006366891e5f39baf6c48b4076bdce36897
frustratedcat <jaredbrown@skiff.com>
2023-12-29 10:54:35 -0500

    Update to hide open modal when other modal is opened

--- src/modules/dom-handling.js
+++ src/modules/dom-handling.js
@@ -1,38 +1,58 @@
 // Import functions
 import { createToDo, createProject, createNewProject } from "./logic-handling";
 
+// Define all DOM objects
 function defineDOMItems() {
+  // Btns to open form modals
+  const addProjectBtn = document.getElementById("add-project-btn");
+  const addToDoBtn = document.getElementById("add-to-do-btn");
+
+  // Form modal objects
   const addProjectForm = document.getElementById("add-project-form");
   const addToDoForm = document.getElementById("add-to-do-form");
 
-  return { addProjectForm, addToDoForm }
+  // Btns to submit form modals
+  const addProjectSubmitBtn = document.getElementById("add-project-submit-btn");
+  const addToDoSubmitBtn = document.getElementById("add-to-do-submit-btn");
+
+  return { addProjectForm, addToDoForm, addProjectBtn, addToDoBtn, addProjectSubmitBtn, addToDoSubmitBtn }
 }
 
 // Add click event to add-project-btn
 function clickAddProjectBtn() {
-  const addProjectBtn = document.getElementById("add-project-btn");
+  const addProjectBtn = defineDOMItems().addProjectBtn;
   addProjectBtn.addEventListener("click", (e) => {
     console.log(e.target);
 
+    // Show projects form modal
     const addProjectForm = defineDOMItems().addProjectForm;
-    addProjectForm.classList.remove('hide-form');
+    addProjectForm.classList.remove("hide-form");
+
+    // Hide To-Do form modal
+    const addToDoForm = defineDOMItems().addToDoForm;
+    addToDoForm.classList.add("hide-form");
   })
 }
 
 // Add click event to add-to-do-btn
 function clickAddToDoBtn() {
-  const addToDoBtn = document.getElementById("add-to-do-btn");
+  const addToDoBtn = defineDOMItems().addToDoBtn;
   addToDoBtn.addEventListener("click", (e) => {
     console.log(e.target);
 
+    // Show To-Do form modal
     const addToDoForm = defineDOMItems().addToDoForm;
     addToDoForm.classList.remove('hide-form');
+
+    // Hide projects form modal
+    const addProjectForm = defineDOMItems().addProjectForm;
+    addProjectForm.classList.add("hide-form");
   })
 }
 
 // Submit new project form
 function submitProjectForm() {
-  const addProjectSubmitBtn = document.getElementById("add-project-submit-btn");
+  const addProjectSubmitBtn = defineDOMItems().addProjectSubmitBtn;
   addProjectSubmitBtn.addEventListener("click", (e) => {
     console.log(e.target);
 
@@ -45,7 +65,7 @@ function submitProjectForm() {
 
 // Submit new to-do form
 function submitToDoForm() {
-  const addToDoSubmitBtn = document.getElementById("add-to-do-submit-btn");
+  const addToDoSubmitBtn = defineDOMItems().addToDoSubmitBtn;
   addToDoSubmitBtn.addEventListener("click", (e) => {
     console.log(e.target);
 
