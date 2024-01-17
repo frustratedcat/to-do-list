28d7298f5b31fd63fd944355474c8a9ad07236cb
frustratedcat <jaredbrown@skiff.com>
2024-01-17 15:05:46 -0500

    Update styling

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,4 @@
 const path = require('path');
-const toml = require('toml');
-const yaml = require('yamljs');
 const json5 = require('json5');
 
 module.exports = {
@@ -10,39 +8,22 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
-module: {
+  module: {
     rules: [
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
- {
+      {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
       },
-	{
+      {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: 'asset/resource',
       }, {
         test: /\.(csv|tsv)$/i,
         use: ['csv-loader'],
-      },
-      {
-        test: /\.xml$/i,
-        use: ['xml-loader'],
-      },  {
-        test: /\.toml$/i,
-        type: 'json',
-        parser: {
-          parse: toml.parse,
-        },
-      },
-      {
-        test: /\.yaml$/i,
-        type: 'json',
-        parser: {
-          parse: yaml.parse,
-        },
       },
       {
         test: /\.json5$/i,
